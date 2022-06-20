7d24454e29864643b8035aada81ccff367144bd9
chientom <chienfizz@gmail.com>
2022-06-20 20:53:56 +0700

    hoan thien admin

---
+++ Admin/giamdoc.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Giám đốc</title>
+	<style type="text/css">
+		img {
+			width: 30%;
+			height: 20%;
+			margin-left: 35%;
+			border-radius: 2em;
+		}
+	</style>
+</head>
+<body>
+	<img src="anhgiamdoc.jpg">
+</body>
+</html>
